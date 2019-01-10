dae46274a3615b23b079de66c6f931de01258f59
pajouheshgar <pajouheshgar@ce.sharif.edu>
2019-01-10 14:57:51 +0330

    fixed gan dataloader and added conditional gan dataloader

--- Code/Dataloaders.py
+++ Code/Dataloaders.py
@@ -1,11 +1,9 @@
 import os
-import imageio
+import json
 import numpy as np
 from PIL import Image
-from scipy import misc
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -19,13 +17,23 @@ flags.DEFINE_integer('image_height', 64, 'Height of images')
 flags.DEFINE_integer('image_width', 64, 'Width of images')
 
 
-class GAN_dataloader():
-    DIRECTORIES_TO_INCLUDE = ['apple', 'facebook', 'google', 'twitter', 'messenger']
+class GAN_Dataloader():
+    COMPANIES = ['apple', 'facebook', 'google', 'twitter', 'messenger']
+    CATEGORIES = ['Food & Drink', 'Smileys & People', 'Symbols', 'Travel & Places', 'Skin Tones', 'Activities',
+                  'Objects', 'Animals & Nature', 'Flags']
+    company2id = {c: i for i, c in enumerate(COMPANIES)}
+    id2company = {i: c for i, c in enumerate(COMPANIES)}
+    category2id = {c: i for i, c in enumerate(CATEGORIES)}
+    id2category = {i: c for i, c in enumerate(CATEGORIES)}
 
-    def __init__(self, directories_to_include=DIRECTORIES_TO_INCLUDE):
-        self.directories_to_include = directories_to_include
+    COMPANIES_TO_INCLUDE = ['apple', 'facebook', 'google', 'twitter', 'messenger']
+    CATEGORIES_TO_INCLUDE = ['Smileys & People']
+
+    def __init__(self, companies_to_include=COMPANIES_TO_INCLUDE):
+        self.companies_to_include = companies_to_include
         self.files_name_list = []
-        for directory in self.directories_to_include:
+
+        for directory in self.companies_to_include:
             images_in_directory = list(filter(lambda s: ".png" in s,
                                               os.listdir(FLAGS.dataset_dir + "emoji-images/" + directory + "/")))
             labels = [directory for _ in images_in_directory]
@@ -40,8 +48,8 @@ class GAN_dataloader():
         for image_file_name, label in self.files_name_list:
             emoji_image = Image.open(FLAGS.dataset_dir + "emoji-images/{}/{}".format(label, image_file_name)).convert(
                 "RGBA")
-            emoji_image = emoji_image.resize([FLAGS.image_height, FLAGS.image_width], Image.ANTIALIAS)
-            yield emoji_image, self.directories_to_include.index(label)
+            emoji_image = np.array(emoji_image.resize([FLAGS.image_height, FLAGS.image_width], Image.ANTIALIAS)) / 255.0
+            yield emoji_image, self.company2id[label]
 
     def _create_dataset(self, generator):
         output_shapes = (tf.TensorShape([FLAGS.image_height, FLAGS.image_width, 4]), tf.TensorShape([]))
@@ -55,17 +63,107 @@ class GAN_dataloader():
         return dataset
 
 
+class Conditional_GAN_Dataloader():
+    COMPANIES = ['apple', 'facebook', 'google', 'twitter', 'messenger']
+    CATEGORIES = ['Food & Drink', 'Smileys & People', 'Symbols', 'Travel & Places', 'Skin Tones', 'Activities',
+                  'Objects', 'Animals & Nature', 'Flags']
+
+    COMPANIES_TO_INCLUDE = ['apple', 'facebook', 'google', 'twitter', 'messenger']
+    CATEGORIES_TO_INCLUDE = ['Smileys & People']
+
+    def __init__(self, companies_to_include=COMPANIES_TO_INCLUDE, categories_to_include=CATEGORIES_TO_INCLUDE):
+        self.companies_to_include = companies_to_include
+        self.categories_to_include = categories_to_include
+        self.company2id = {c: i for i, c in enumerate(self.companies_to_include)}
+        self.id2company = {i: c for i, c in enumerate(self.companies_to_include)}
+        self.category2id = {c: i for i, c in enumerate(self.categories_to_include)}
+        self.id2category = {i: c for i, c in enumerate(self.categories_to_include)}
+        self.files_name_list = []
+
+        with open(FLAGS.dataset_dir + "emoji_pretty.json") as f:
+            self.additional_data = json.load(f)
+        self.img_name_2_info = {d['image']: d for d in self.additional_data}
+
+        for directory in self.companies_to_include:
+            images_in_directory = list(
+                filter(lambda s: (".png" in s) and (s in self.img_name_2_info) and (self.img_name_2_info[s][
+                                                                                        'category'] in self.categories_to_include),
+                       os.listdir(FLAGS.dataset_dir + "emoji-images/" + directory + "/")))
+            labels = [directory for _ in images_in_directory]
+            self.files_name_list += (list(zip(images_in_directory, labels)))
+
+        train_dataset = self._create_dataset(self._generator)
+        train_iterator = train_dataset.make_initializable_iterator()
+        self.train_initializer = train_iterator.initializer
+        self.train_batch = train_iterator.get_next()
+
+    def _generator(self):
+        for image_file_name, label in self.files_name_list:
+            emoji_image = Image.open(FLAGS.dataset_dir + "emoji-images/{}/{}".format(label, image_file_name)).convert(
+                "RGBA")
+            emoji_image = np.array(emoji_image.resize([FLAGS.image_height, FLAGS.image_width], Image.ANTIALIAS)) / 255.0
+            company_id = self.company2id[label]
+            category_id = self.category2id[self.img_name_2_info[image_file_name]['category']]
+            yield emoji_image, company_id, category_id
+
+    def _create_dataset(self, generator):
+        output_shapes = (
+            tf.TensorShape([FLAGS.image_height, FLAGS.image_width, 4]), tf.TensorShape([]), tf.TensorShape([]))
+        dataset = tf.data.Dataset.from_generator(generator=generator,
+                                                 output_types=(tf.float32, tf.int32, tf.int32),
+                                                 output_shapes=output_shapes
+                                                 )
+
+        # dataset = dataset.shuffle(buffer_size=FLAGS.shuffle_buffer_size, seed=42).repeat(1).batch(
+        dataset = dataset.batch(FLAGS.batch_size)
+        return dataset
+
+
+# if __name__ == "__main__":
+#     dataloader = GAN_Dataloader(companies_to_include=['apple'])
+#     x_train, y_train = dataloader.train_batch
+#     ses = tf.InteractiveSession()
+#     ses.run(dataloader.train_initializer)
+#
+#     a = ses.run(x_train)
+#     print(a.shape)
+#     # plt.imshow(np.array(a[0], dtype=np.uint8))
+#     plt.imshow(a[0] / 255)
+#     print(a[0].mean())
+#     plt.show()
+#
+#     # dataloader = noisy_classification_dataloader(flatten=False)
+#     # x_test, y_test = dataloader.test_batch
+#     # x_validation, y_validation = dataloader.validation_batch
+#     # i = 0
+#     #
+#     # while True:
+#     #     try:
+#     #         a, b = ses.run([x_train, y_train])
+#     #         print(a.shape)
+#     #         plt.figure()
+#     #         plt.imshow(a[0, :, :, 0])
+#     #         i += 1
+#     #         if i == 10:
+#     #             break
+#     #
+#     #     except tf.errors.OutOfRangeError:
+#     #         print("reinitializing dataset")
+#     #         ses.run(dataloader.train_initializer)
+#     #         i = 0
+#     #
+#     # plt.show()
+
 if __name__ == "__main__":
-    dataloader = GAN_dataloader(directories_to_include=['apple'])
-    x_train, y_train = dataloader.train_batch
+    dataloader = Conditional_GAN_Dataloader(companies_to_include=['apple'], categories_to_include=['Smileys & People'])
+    x_train, y_train, z_train = dataloader.train_batch
     ses = tf.InteractiveSession()
     ses.run(dataloader.train_initializer)
 
     a = ses.run(x_train)
     print(a.shape)
     # plt.imshow(np.array(a[0], dtype=np.uint8))
-    plt.imshow(a[0] / 255)
-    print(a[0].mean())
+    plt.imshow(a[0])
     plt.show()
 
     # dataloader = noisy_classification_dataloader(flatten=False)
--- Code/test.py
+++ Code/test.py
@@ -1,4 +1,35 @@
+import os
+import json
+
+from collections import defaultdict
 import tensorflow as tf
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
+flags.DEFINE_string('dataset_dir', "../Dataset/", 'Directory of dataset')
+
+with open(FLAGS.dataset_dir + "emoji_pretty.json") as f:
+    data = json.load(f)
+
+COMPANIES_TO_INCLUDE = ['apple', 'facebook', 'google', 'twitter', 'messenger']
+CATEGORIES_TO_INCLUDE = ['Food & Drink', 'Smileys & People', 'Symbols', 'Travel & Places', 'Skin Tones', 'Activities',
+                         'Objects', 'Animals & Nature', 'Flags']
+
+
+
+print(data[0])
+print(len(data))
+files_name_list = []
+img_name_2_inf = {d['image']: d for d in data}
+
+
+for directory in COMPANIES_TO_INCLUDE:
+    images_in_directory = list(filter(lambda s: ".png" in s,
+                                      os.listdir(FLAGS.dataset_dir + "emoji-images/" + directory + "/")))
+    # for img in images_in_directory:
+    #     if img not in img_name_2_inf:
+    #         print(directory + "/" + img)
+    labels = [directory for _ in images_in_directory]
+    files_name_list += (list(zip(images_in_directory, labels)))
+
+print(files_name_list)
